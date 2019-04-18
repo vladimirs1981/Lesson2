298eec492160c85b112d24e794cad593faf10e82
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-04-18 18:41:28 +0200

    Countdown

--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -8,4 +8,12 @@ package exercises;
 
 public class NASACountdown {
 
+	public static void main(String[] args) {
+		
+		for (int i = 10; i>0; i--) {
+			System.out.println(i);
+		}
+		System.out.println("Blastoff!!!");
+	}
+	
 }
