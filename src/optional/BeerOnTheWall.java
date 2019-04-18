d990b3e4470b6a4a315c84bd17adc97e2243bcb8
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-04-18 18:43:30 +0200

    Beeeergit status!

--- src/optional/BeerOnTheWall.java
+++ src/optional/BeerOnTheWall.java
@@ -14,17 +14,18 @@ public class BeerOnTheWall {
 		
 		for (int i = 99; i >=1; i--) {
 				
-			
-			System.out.println(i+ " bottles of beer on the wall,"+ i+" bottles of beer.\n"
-					+ "Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.");
-			if (i==2 &&(i-1)==1) {
+		
+			if (i==2 && (i-1)==1) {
 				System.out.println("2 bottles of beer on the wall, 2 bottles of beer.\r\n" + 
 						"Take one down and pass it around, 1 bottle of beer on the wall.");
 			}else if(i==1&&(i-1)==0) {
 					System.out.println("1 bottle of beer on the wall, 1 bottle of beer.\r\n" + 
 							"Take one down and pass it around, no more bottles of beer on the wall.");
-				}
+				}else{
+					System.out.println(i+ " bottles of beer on the wall,"+ i+" bottles of beer.\n"
+						+ "Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.");
 			}
+		}
 			System.out.println("No more bottles of beer on the wall, no more bottles of beer.\r\n" + 
 					"Go to the store and buy some more, 99 bottles of beer on the wall.");
 		}
