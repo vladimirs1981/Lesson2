474450030e009e81ab892bc569e4427666fe8288
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-04-18 18:45:10 +0200

    Robot

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -8,11 +8,28 @@ public class IntroToRobot {
 		Robot r2d2 = new Robot();
 		r2d2.penDown();
 		r2d2.setPenWidth(20);
-		r2d2.setSpeed(5);
+		r2d2.setSpeed(10);
 		for (int i = 0; i < 4; i++) {
+			r2d2.setRandomPenColor();
 			r2d2.move(100);
 			r2d2.turn(90);
 		}
-
+		for (int i = 0; i < 4; i++) {
+			r2d2.setRandomPenColor();
+			r2d2.turn(90);
+			r2d2.move(100);
+		}
+		r2d2.turn(180);
+		for (int i = 0; i < 4; i++) {
+			r2d2.setRandomPenColor();
+			r2d2.move(100);
+			r2d2.turn(90);
+		}
+		r2d2.turn(90);
+		for (int i = 0; i < 4; i++) {
+			r2d2.setRandomPenColor();
+			r2d2.move(100);
+			r2d2.turn(90);
+		}
 	}
 }
